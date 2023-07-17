1df296bd1db64e9cb6f71db76cda7d2485abecd2
vidya <vidyashivakumar13@gmail.com>
2023-07-17 18:55:29 +0530

    corrected move function

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -14,14 +14,14 @@ public class Vehicle {
 
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
-        this.currentDirection = currentDirection+direction;
+        currentDirection = currentDirection+direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
-        this.currentSpeed = currentSpeed;
-        this.currentDirection = currentDirection;
+        this.currentSpeed = speed;
+        this.currentDirection = direction;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
